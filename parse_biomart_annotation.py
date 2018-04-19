1bb28a01ce9d99b72bab58c02c035df7f07e6914
heyyyjude <joowookbio@gmail.com>
2018-04-19 10:24:00 +0900

    parsing rsemoutput

--- parse_biomart_annotation.py
+++ parse_biomart_annotation.py
@@ -3,7 +3,7 @@ from namedlist import namedlist
 
 
 def main(anno_file):
-    Data = namedlist("Data", "gene_id symbol desc refseq_mrna refseq_ncrna")
+    Data = namedlist("Data", "Gene_stable_id refseq_ncrna hgnc_symbol description")
     results = OrderedDict()
     first = ""
     with open(anno_file, 'rt')as fin:
@@ -12,32 +12,28 @@ def main(anno_file):
             line = line.strip("\n")
             csv_list = line.split("\t")
             ensembl_id = csv_list[0]
-            desc = csv_list[1]
-            symbol = csv_list[4]
-            mrna = csv_list[2]
-            ncrna = csv_list[3]  # tmp[3] hgnc_symbol == gene_name
+            ncrna = csv_list[1]
+            symbol = csv_list[2]
+            desc = csv_list[3]
 
             if ensembl_id in results:
                 tmp = results[ensembl_id]
-                if mrna != "":
-                    tmp.refseq_mrna +=  mrna + ";"
                 if ncrna != "":
                     tmp.refseq_ncrna += ncrna + ";"
             else:
-                results[ensembl_id] = Data(gene_id=ensembl_id, symbol=symbol, desc=desc, refseq_mrna=mrna,
-                                           refseq_ncrna=ncrna)
+                results[ensembl_id] = Data(Gene_stable_id=ensembl_id, refseq_ncrna=ncrna, hgnc_symbol=symbol,
+                                           description=desc)
 
     with open(anno_file + ".pared.tsv", 'wt')as fout:
-        fout.write("Gene stable ID\tGene name\tdescription\tRefSeq mRNA ID\tRefSeq ncRNA ID\n")
+        fout.write("HGNC symbol\tGene stable ID\tRefSeq ncRNA ID\tdescription\n")
         for i in results:
             tmp = results[i]
             tab = "\t"
-            tmp.refseq_mrna = tmp.refseq_mrna.rstrip(";")
             tmp.refseq_ncrna = tmp.refseq_ncrna.rstrip(";")
-            wf = tmp.gene_id + tab + tmp.symbol + tab + tmp.desc + tab + tmp.refseq_mrna + tab + tmp.refseq_ncrna + "\n"
+            wf = tmp.hgnc_symbol + tab + tmp.Gene_stable_id +tab + tmp.refseq_ncrna + tab + tmp.description + "\n"
             fout.write(wf)
 
 
 if __name__ == '__main__':
-    anno_f = r"D:\mRNAseq\mart_export_GRCh37.tsv"
+    anno_f = r"E:\work\mRNA-seq\annotation\BiomaRt.GRCh37.p13.anno.gz.txt"
     main(anno_f)
